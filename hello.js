6bb5d86f90b5419436a77d2d2da3e44db6c4d8e1
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-11-13 16:39:47 +0600

    javascript function

--- hello.js
+++ hello.js
@@ -1,18 +1,11 @@
-var num = 100;
-switch(num){
-    case 1000:
-        console.log("i am 1000");
-        break;
-    case 100:
-        console.log("i am 100");
-        break;
-    case 10:
-        console.log("i am 10")
-        break;
-    case 20:
-    case 5:
-        console.log("i am 20 & 5")
-        break;
-    default:
-        console.log("I don't know the number")
+function sayAllhamdulliah(){
+    console.log("Allhamdulillah");
+    console.log("Allhamdulillah, sokol proshongsha Allahor");
 }
+
+sayAllhamdulliah();
+var date = "all time";
+var where = "all place";
+sayAllhamdulliah();
+var when = "every  time";
+sayAllhamdulliah();
