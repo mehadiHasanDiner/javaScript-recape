5cfccb25eb5f34f4f34083876d5a388f2bafdc07
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-11-13 16:30:31 +0600

    javascript switch

--- hello.js
+++ hello.js
@@ -1,7 +1,18 @@
-
-var friends = ["aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh"]
-
-for(i = 0; i < friends.length; i++){
-    var element = friends[i];
-    console.log(element);
+var num = 100;
+switch(num){
+    case 1000:
+        console.log("i am 1000");
+        break;
+    case 100:
+        console.log("i am 100");
+        break;
+    case 10:
+        console.log("i am 10")
+        break;
+    case 20:
+    case 5:
+        console.log("i am 20 & 5")
+        break;
+    default:
+        console.log("I don't know the number")
 }
