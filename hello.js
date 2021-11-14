675a3bf72ee22dac8a2f757bf9869a4e12ba3d91
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-11-14 18:13:39 +0600

    object

--- hello.js
+++ hello.js
@@ -14,6 +14,12 @@ var phoneNo3 = student1[phonePropName];
 
 // object a proparty update korar niyom;
 student2.phone = 1584785;
+student2["phone"] = 1985666;
+student2[phonePropName]=16554566;
+
+student2.cinema = "Ogni22";
+student2.hero = "Hero alam";
+student2["hero"] = "Shakib khan"
 
 console.log(phoneNo1);
 console.log(student2)
