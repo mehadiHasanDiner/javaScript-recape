a16d6b65e89f50140b776afd5373668011445a78
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-11-20 12:17:23 +0600

    for loop recape

--- hello.js
+++ hello.js
@@ -1,16 +1,8 @@
 
-function dobuleIt(num){
-    var result = num * 2;
-    return result;
+var salary = [55, 65, 67, 89, 89, 78, 64, 56]
+
+for(var i = 0; i < salary.length; i++){
+    var element = salary[i];
+    console.log(element);
 }
-
-var num1 = dobuleIt (3);
-var num2 = dobuleIt (5);
-var num3 = dobuleIt (6);
-var num4 = dobuleIt (7);
-var num5 = dobuleIt (8);
-var num6 = dobuleIt (9);
-
-var total = num1 + num2 + num3 + num4+ num5 + num6;
-console.log(total);
 
