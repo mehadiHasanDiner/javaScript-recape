4a2fa4ca41e7792715b4d07a9a07df99666557d6
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-11-20 11:07:47 +0600

    function return update

--- hello.js
+++ hello.js
@@ -7,7 +7,10 @@ function dobuleIt(num){
 var num1 = dobuleIt (3);
 var num2 = dobuleIt (5);
 var num3 = dobuleIt (6);
+var num4 = dobuleIt (7);
+var num5 = dobuleIt (8);
+var num6 = dobuleIt (9);
 
-var total = num1 + num2 + num3;
+var total = num1 + num2 + num3 + num4+ num5 + num6;
 console.log(total);
 
