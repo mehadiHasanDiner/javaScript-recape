87ded3d1ec4bf3f3fe159a53b820223b8a1042da
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-11-22 17:56:09 +0600

    function lowest

--- hello.js
+++ hello.js
@@ -1,8 +1,15 @@
 
-function largestNumber(numbers){
-    for(var i = 0; i < numbers.length; i++){
-        var element = numbers[i];
-        console.log(element);
+function largestNumber(number){
+    var lowest = number[0]
+    for(var i = 0; i < number.length; i++){
+        var element = number[i];
+        if(lowest > element){
+            lowest = number;
+        }
     }
+    return lowest;
+
 }
-largestNumber([56, 34, 40, 90]);
+var result = [12, 45, 36, 58, 98]
+var total = largestNumber(result);
+console.log(total);
