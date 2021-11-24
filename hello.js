aba3d8f0273434f765110953d4eb736940e56ee2
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-11-24 10:10:49 +0600

    function how serch big number

--- hello.js
+++ hello.js
@@ -1,15 +1,15 @@
 
-function lowestNumberOf(number){
+function highestNumber(number){
     var highest = number[0]
     for(var i = 0; i < number.length; i++){
         var element = number[i];
         if(element > number){
-            number = highest;
+            element = highest;
         }
     }
-    return highest;
+    return element;
 
 }
 var result = [203, 333, 564, 698]
-var total = lowestNumberOf(result);
+var total = highestNumber(result);
 console.log(total);
